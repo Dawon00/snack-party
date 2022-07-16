204202bb6c0e49744d2cf6b8fd36498297c46711
Hyeonjae-K <href.hjk@gmail.com>
2022-07-16 19:28:52 +0900

    FIX - input field padding 및 login button overlayColor 수정

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -18,86 +18,81 @@ class _LoginScreenState extends State<LoginScreen> {
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Flexible(
-              flex: 2,
-              child: Container(),
-            ),
-            // logo
-            Image.network(
-              'https://upload.wikimedia.org/wikipedia/en/thumb/3/37/MapleStory.SVG/1200px-MapleStory.SVG.png',
-              height: 64,
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            // email input field
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              // logo
+              Image.network(
+                'https://upload.wikimedia.org/wikipedia/en/thumb/3/37/MapleStory.SVG/1200px-MapleStory.SVG.png',
+                height: 64,
+              ),
+              // email input field
+              TextField(
                 decoration: const InputDecoration(
                   hintText: '메일주소',
                 ),
                 keyboardType: TextInputType.emailAddress,
                 controller: _emailController,
               ),
-            ),
-            // password input field
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
+              // password input field
+              TextField(
                 decoration: const InputDecoration(
                   hintText: '비밀번호',
                 ),
                 controller: _passwordController,
                 obscureText: true,
               ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            // login button
-            TextButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.blue),
+              const SizedBox(
+                height: 24,
               ),
-              child: const Text(
-                '이어하기',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            // signup button
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => SignupScreen(),
+              // login button
+              TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  overlayColor: MaterialStateProperty.all(
+                    Colors.white.withOpacity(0.1),
                   ),
-                );
-              },
-              child: const Text(
-                '가입하기',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 16,
+                ),
+                child: const Text(
+                  '이어하기',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
                 ),
               ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(),
-            ),
-          ],
+              // signup button
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => SignupScreen(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  '가입하기',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+            ],
+          ),
         ),
       ),
     );
