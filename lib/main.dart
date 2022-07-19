504b448f3cd9e58464ae70429553c4164dd79442
Hyeonjae-K <href.hjk@gmail.com>
2022-07-20 01:00:50 +0900

    FEAT - 메인 페이지 로그인 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:snackparty/screen/home_screen.dart';
-import 'package:snackparty/screen/my_info_screen.dart';
-import 'package:snackparty/screen/my_party_screen.dart';
-import 'package:snackparty/widget/bottom_bar.dart';
+import 'package:snackparty/screen/login_screen.dart';
+// import 'package:snackparty/screen/my_info_screen.dart';
+// import 'package:snackparty/screen/my_party_screen.dart';
+// import 'package:snackparty/screen/signup_screen.dart';
+// import 'package:snackparty/widget/bottom_bar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 //import 'package:provider/provider.dart';
@@ -27,21 +30,47 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Snack Party',
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: [
-              HomeScreen(),
-              MyPartyScreen(),
-              MyInfoScreen(),
-            ],
-          ),
-          bottomNavigationBar: Bottom(),
-        ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // 데이터가 들어온 경우
+          if (snapshot.connectionState == ConnectionState.active) {
+            // 데이터가 정상인 경우 HomeScreen 반환
+            if (snapshot.hasData) {
+              return const HomeScreen();
+            } else if (snapshot.hasError) {
+              // 데이터에 에러가 있는 경우 error Text 페이지 반환
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            }
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            // 데이터가 들어오는 중인 경우 CircularProgressIndecator 반환
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          // 데이터가 없는 경우 LoginScreen 반환
+          return const LoginScreen();
+        },
       ),
+      // home: DefaultTabController(
+      //   length: 3,
+      //   child: Scaffold(
+      //     body: TabBarView(
+      //       physics: NeverScrollableScrollPhysics(),
+      //       children: [
+      //         HomeScreen(),
+      //         MyPartyScreen(),
+      //         // MyInfoScreen(),
+      //         LoginScreen(),
+      //       ],
+      //     ),
+      //     bottomNavigationBar: Bottom(),
+      //   ),
+      // ),
     );
   }
 }
