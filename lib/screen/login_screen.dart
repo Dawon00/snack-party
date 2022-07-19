5c018833911d1ba588e31752e90d90297d445a4d
Hyeonjae-K <href.hjk@gmail.com>
2022-07-20 00:33:53 +0900

    FEAT - 로그인 기능 구현

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:snackparty/screen/home_screen.dart';
 import 'package:snackparty/screen/signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,6 +13,50 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      String email = _emailController.text;
+      String password = _passwordController.text;
+
+      // 모든 필드 값이 isNotEmpty일 경우 로그인 진행
+      if (email.isNotEmpty && password.isNotEmpty) {
+        // 로그인 진행
+        await auth.signInWithEmailAndPassword(email: email, password: password);
+
+        // HomeScreen으로 화면 전환
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: ((context) => const HomeScreen()),
+          ),
+        );
+      } else {
+        // 필드 값이 비어있는 경우 SnackBar에 필드 입력 메시지 출력
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Please enter all the fields"),
+          ),
+        );
+      }
+    } catch (err) {
+      // 오류 발생시 SnackBar에 오류 출력
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(err.toString()),
+        ),
+      );
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +78,9 @@ class _LoginScreenState extends State<LoginScreen> {
                 'https://upload.wikimedia.org/wikipedia/en/thumb/3/37/MapleStory.SVG/1200px-MapleStory.SVG.png',
                 height: 64,
               ),
+              const SizedBox(
+                height: 12,
+              ),
               // email input field
               TextField(
                 decoration: const InputDecoration(
@@ -49,51 +98,76 @@ class _LoginScreenState extends State<LoginScreen> {
                 obscureText: true,
               ),
               const SizedBox(
-                height: 24,
+                height: 12,
               ),
               // login button
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  overlayColor: MaterialStateProperty.all(
-                    Colors.white.withOpacity(0.1),
-                  ),
-                ),
-                child: const Text(
-                  '이어하기',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-              // signup button
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => const SignupScreen(),
+              InkWell(
+                onTap: loginUser,
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
                     ),
-                  );
-                },
-                child: const Text(
-                  '가입하기',
-                  style: TextStyle(
                     color: Colors.blue,
-                    fontSize: 16,
                   ),
+                  child: !_isLoading
+                      ? const Text(
+                          'Log in',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      : const CircularProgressIndicator(
+                          color: Colors.white,
+                        ),
                 ),
               ),
               Flexible(
                 flex: 2,
                 child: Container(),
               ),
+              // signup button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text(
+                      "Don't have an account?",
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const SignupScreen(),
+                      ),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text(
+                        ' Signup.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
   }
 }
